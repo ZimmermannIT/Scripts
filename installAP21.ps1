3d98ba99a28877d51804453f9fa4a62778a92032
stevenm57811 <105845131+stevenm57811@users.noreply.github.com>
2023-10-16 15:32:55 +1100

    Update installAP21.ps1

--- installAP21.ps1
+++ installAP21.ps1
@@ -5,13 +5,13 @@ Invoke-WebRequest -Uri "https://zimazfilestorage.blob.core.windows.net/scripts/A
 
 & 'C:\Windows\Temp\Ap21.exe'/S /SyncOnStartup=1 /SyncLive=1 /SyncTest=1 /SyncNow=0 /ServerName=ap21db
 
+start-sleep -Seconds 400
+
 $shell = New-Object -comObject WScript.Shell
 $shortcut = $shell.CreateShortcut("C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\Sync.lnk")
 $shortcut.TargetPath = "C:\Program Files (x86)\Apparel 21\Apparel 21 ERP\sync.exe"
 $shortcut.WorkingDirectory = "C:\Program Files (x86)\Apparel 21\Apparel 21 ERP"
 $shortcut.Save()
-
-start-sleep -Seconds 400
 
 Write-Output "Ap21 installed and startup shortcut created"
 
