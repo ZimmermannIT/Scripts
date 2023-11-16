44b47e35e167229d29f61d2c1dbbccd71a8a4fda
stevenm57811 <105845131+stevenm57811@users.noreply.github.com>
2023-11-16 14:02:04 +1100

    Add files via upload

---
+++ ImportUSERDAT.ps1
@@ -0,0 +1,3 @@
+﻿Invoke-WebRequest -Uri "https://zimazfilestorage.blob.core.windows.net/scripts/NTUSER.DAT" -OutFile c:\apps\NTUSER.dat
+
+Copy-Item -Path "c:\apps\NTUSER.dat" -Destination "C:\Users\Default" -Force
