1ad2b947efaedb31527bedd93de4370a9a804cd7
stevenm57811 <105845131+stevenm57811@users.noreply.github.com>
2023-11-17 14:39:38 +1100

    Update installAP21.ps1

--- installAP21.ps1
+++ installAP21.ps1
@@ -1,4 +1,4 @@
-﻿###Installs AP21
+﻿###InstallsAP21
 try
 {
 Invoke-WebRequest -Uri "https://zimazfilestorage.blob.core.windows.net/scripts/Apparel 21 Client Framework 8.12.exe" -OutFile C:\Windows\Temp\Ap21.exe
