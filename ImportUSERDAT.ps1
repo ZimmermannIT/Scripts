bcfcdae273e30f13c2432bfc488c9c2431b0c99f
stevenm57811 <105845131+stevenm57811@users.noreply.github.com>
2023-11-21 10:32:05 +1100

    Update ImportUSERDAT.ps1

--- ImportUSERDAT.ps1
+++ ImportUSERDAT.ps1
@@ -1,5 +1,7 @@
 New-Item -Type Directory -Path C:\apps2
 
-Invoke-WebRequest -Uri "https://zimazfilestorage.blob.core.windows.net/scripts/NTUSER.DAT" -OutFile c:\apps\NTUSER.dat
+Invoke-WebRequest -Uri "https://zimazfilestorage.blob.core.windows.net/scripts/NTUSER.DAT" -OutFile c:\apps2\NTUSER.dat
 
-Copy-Item -Path "c:\apps\NTUSER.dat" -Destination "C:\Users\Default" -Force
+Copy-Item -Path "c:\apps2\NTUSER.dat" -Destination "C:\Users\Default" -Force
+
+remove-item -Path "C:\apps2" -Force -Recurse
